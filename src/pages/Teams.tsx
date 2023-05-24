cae676faea11494819080d442d904c00a396324b
Lucas <lucas_emanuel_sc@hotmail.com>
2023-05-24 13:38:37 -0300

    feat: first test

--- src/pages/Teams.tsx
+++ src/pages/Teams.tsx
@@ -1,17 +1,18 @@
-import { useParams } from 'react-router-dom';
-import ITeam from '../interfaces/ITeam'
-import useFetch from '../hooks/useFetchWithParams';
+// import { useParams } from 'react-router-dom';
+// import ITeam from '../interfaces/ITeam'
+// import useFetch from '../hooks/useFetchWithParams';
 import TeamsCard from '../components/TeamCard';
+import { teams } from '../components/teamMock';
 
 function Teams() {
-  const TEAMS_URL = 'https://v3.football.api-sports.io/teams'
-    const { id, season } = useParams();
-  const params = {
-    league: Number(id),
-    season: Number(season)
-  }
+  // const TEAMS_URL = 'https://v3.football.api-sports.io/teams'
+  //   const { id, season } = useParams();
+  // const params = {
+  //   league: Number(id),
+  //   season: Number(season)
+  // }
 
-  const { data: teams, isLoading, errorMessage } = useFetch<ITeam[]>(TEAMS_URL, params)
+  // const { data: teams, isLoading, errorMessage } = useFetch<ITeam[]>(TEAMS_URL, params)
   return (
     <div>
       {
@@ -21,8 +22,8 @@ function Teams() {
           )
         })
       }
-    { errorMessage && <h5>{ errorMessage }</h5> }
-    { isLoading && <p>Loading...</p> }
+    {/* { errorMessage && <h5>{ errorMessage }</h5> }
+    { isLoading && <p>Loading...</p> } */}
     </div>
   )
 }
